28de68932999b3b4efad93e9ef5b305efe7ae785
FHRha <80634532+FHRha@users.noreply.github.com>
2026-07-19 16:15:02 +0500

    fix(i18n): use BASE_URL for localization loadPath to support Nginx routing

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -14,7 +14,7 @@ i18n
       escapeValue: false, // React already safe from xss
     },
     backend: {
-      loadPath: './locales/{{lng}}/{{ns}}.json',
+      loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/{{ns}}.json`.replace('//', '/'),
     },
   });
 
